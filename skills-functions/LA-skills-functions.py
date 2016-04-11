62baf7501ffb2b569a7f2e48d76ef59df25b776f
Lianne Achilles <lachilles@Liannes-iMac.local>
2016-04-10 19:21:03 -0700

    Implemented hello, multiply, repeat, and zero functions

--- skills-functions/LA-skills-functions.py
+++ skills-functions/LA-skills-functions.py
@@ -1,9 +1,54 @@
 
-def helloworld():
+# def helloworld():
 
-	"""Prints Hello World"""
+# 	"""Prints Hello World"""
 
-	print "Hello World"
+# 	print "Hello World"
 
-helloworld()
+# helloworld()
+
+
+# def hello():
+
+# 	"""Prints Hi (name)"""
+
+# 	name = raw_input("What's your name? ")
+# 	print "Hi " + name
+
+# hello()
+
+
+# def multiply(num1, num2):
+	
+# 	""" Multiplies num1 to num2 """
+
+# 	print int(num1) * int(num2)
+
+# multiply(2, 3)
+
+
+def repeat(x, y):
+
+	"""Repeat x y times"""
+	print str(x) * int(y)
+
+
+repeat("hello", 5)
+
+# def zero(x):
+
+# 	"""Evaluate whether if an integer is higher or lower than 0"""
+
+# 	if int(x) > 0:
+# 		print "Higher than 0"
+# 	elif int(x) < 0:
+# 		print "Lower than 0"
+# 	elif int(x) == 0: 
+# 		print "Zero"
+# 	else:
+# 		print "I don't understand that"
+
+# zero(5)
+
+
 
